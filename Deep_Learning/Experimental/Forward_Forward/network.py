93f85ad222b08da7f23fd9becfbede708990a52c
Joe Griffith <joeagriffith@gmail.com>
2023-08-14 11:15:01 +0100

    Learning!

--- Deep_Learning/Experimental/Forward_Forward/network.py
+++ Deep_Learning/Experimental/Forward_Forward/network.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Feedforward(nn.Module):
-    def __init__(self, sizes, activation=nn.ReLU(), bias=True, momentum=0.95):
+    def __init__(self, sizes, activation=nn.ReLU(), bias=True, momentum=0.95, dropout=0.0):
         super().__init__()
         self.in_features = sizes[0]
         self.out_features = sizes[-1]
@@ -11,19 +11,24 @@ class Feedforward(nn.Module):
         self.activation = activation
         self.momentum = momentum
 
+        self.dropout = nn.Dropout(dropout)
         self.layers = nn.ModuleList()
         for i in range(len(sizes)-1):
             self.layers.append(nn.Linear(sizes[i], sizes[i+1], bias=bias))
-        self.target_actvs = [0 for _ in self.layers]
+        self.target_actvs = [None for _ in self.layers]
     
     def update_target_actvs(self, x, layer_i):
-        new_target = x.mean().item()
-        self.target_actvs[layer_i] = self.target_actvs[layer_i]*self.momentum + new_target*(1-self.momentum)
+        if self.target_actvs[layer_i] is None:
+            self.target_actvs[layer_i] = x.mean().item()
+        else:
+            new_target = x.mean().item()
+            self.target_actvs[layer_i] = self.target_actvs[layer_i]*self.momentum + new_target*(1-self.momentum)
 
     def forward(self, x, out_layer=None):
         outs = []
         for i, layer in enumerate(self.layers):
             out = self.activation(layer(F.normalize(x, dim=1)))
+            # out = self.dropout(out)
             outs.append(out)
             self.update_target_actvs(out, i)
             if i == out_layer:
